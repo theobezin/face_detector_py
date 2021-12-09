b68a13b40d5516207089dcb7194773c263a73ab2
Theo Bezin <theo.bezin@supinfo.com>
2021-12-09 17:12:21 +0100

    Working face detection on images

--- Face_Detector.py
+++ Face_Detector.py
@@ -5,17 +5,24 @@ trained_face_data = cv2.CascadeClassifier(
     'haarcascade_frontalface_default.xml')
 
 # importer l'image
-img = cv2.imread('IMG_TEST.jpg')
+img = cv2.imread('IMG_TEST_3.jpg')
 
 
 # convertir en gris
 grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # detecter le visage
-#face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+# print(face_coordinates)
+
+# dessiner un rectangle avec les coordonnées
+for (x, y, w, h) in face_coordinates:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
 
 # afficher l'image
-cv2.imshow('Face Detector Sadeuh', grayscaled_img)
+cv2.imshow('Face Detector Sadeuh', img)
 
 # à mettre avant la fin pour terminer le programme au click
 cv2.waitKey()
